a67d834fa40f1a7e7490749d81f203eb63defff7
Rusty Pickle <rusty.pickle94@gmail.com>
2022-06-20 06:07:25 +0600

    added tx date checking before getting accepted

--- src/add_tx_data.rs
+++ src/add_tx_data.rs
@@ -1,21 +1,28 @@
+use chrono::prelude::{Local};
 use rusqlite::Connection;
-use crate::sub_func::add_new_tx;
+use crate::sub_func::{add_new_tx};
+use std::error::Error;
+
 pub struct AddTxData {
     date: String,
     details: String,
     tx_method: String,
     amount: String,
     tx_type: String,
+    pub tx_status: Vec<String>,
 }
 
 impl AddTxData {
     pub fn new() -> Self {
+        let cu_date = Local::today().to_string();
+        let formatted_cu_date = &cu_date[0..10];
         AddTxData {
-            date: "".to_string(),
+            date: formatted_cu_date.to_string(),
             details: "".to_string(),
             tx_method: "".to_string(),
             amount: "".to_string(),
             tx_type: "".to_string(),
+            tx_status: Vec::new(),
         }
     }
 
@@ -87,5 +94,56 @@ impl AddTxData {
             Err(e) => println!("Error happened {}", e),
         }
         "done".to_string()
+    }
+
+    pub fn add_tx_status(&mut self, data: &str) {
+        if self.tx_status.len() == 20 {
+            self.tx_status.remove(0);
+        }
+        self.tx_status.push(data.to_string());
+    }
+
+    pub fn check_date(&self) -> Result<String, Box<dyn Error>> {
+        let user_date = &self.date;
+
+        if user_date.len() == 0 {
+            return Ok("Date: Nothing to check".to_string());
+        }
+        
+        let splitted = user_date.split("-");
+        let data = splitted.collect::<Vec<&str>>();
+        let int_year: u32 = data[0].parse()?;
+        let int_month: u32 = data[1].parse()?;
+        let int_day: u32 = data[2].parse()?;
+
+        if user_date.len() == 0 {
+            return Ok("Date: Nothing to check".to_string());
+        }
+
+        if data[0].len() != 4 {
+            return Ok("Date: Year Length Not Acceptable. Example Date: 2022-05-01".to_string())
+        }
+
+        if data[1].len() != 2 {
+            return Ok("Date: Month Length Not Acceptable. Example Date: 2022-05-01".to_string())
+        }
+
+        if data[2].len() != 2 {
+            return Ok("Date: Day Length Not Acceptable. Example Date: 2022-05-01".to_string())
+        }
+
+        if int_year < 2022 || int_year > 2025 {
+            return Ok("Date: Year must be between 2022-2025".to_string())
+        }
+
+        if int_month < 1 || int_month > 12 {
+            return Ok("Date: Month must be between 01-12".to_string())
+        }
+
+        if int_day < 1 || int_day > 31 {
+            return Ok("Date: Day must be between 01-31".to_string())
+        }
+
+        Ok("Date: Date Accepted".to_string())
     }
 }
