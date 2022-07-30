a86b72f74e3388e4b292cc1e091e51d938da0dbb
Rusty Pickle <rusty.pickle94@gmail.com>
2022-07-30 19:35:42 +0600

    Test

--- src/initial_page/version_checker.rs
+++ src/initial_page/version_checker.rs
@@ -5,15 +5,15 @@ struct Version {
     name: String,
 }
 
-/// Uses Github API to get the latest version number to check if the current version matches with it.
+/// Uses Github API to get the latest release version number to check if the current version matches with it.
 /// If not, we will start the new version pop up
 pub fn check_version() -> Result<bool, reqwest::Error> {
     let cu_version = "v0.0".to_string();
-    static APP_USER_AGENT: &str = "Testing";
+    static APP_USER_AGENT: &str = "Rex";
     
     let client = reqwest::blocking::Client::builder()
         .user_agent(APP_USER_AGENT)
-        .connect_timeout(std::time::Duration::new(1, 0))
+        .connect_timeout(std::time::Duration::new(2, 0))
         .build()?;
 
     let caller: Version = client.get("https://api.github.com/repos/WaffleMixer/Rex/releases/latest").send()?.json()?;
@@ -21,5 +21,17 @@ pub fn check_version() -> Result<bool, reqwest::Error> {
         return Ok(true)}
     else {
         return Ok(false)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::check_version;
+
+    #[test]
+    // fails as there is no release posted
+    fn version_checker_fails() {
+        let result = check_version().unwrap_err();
+        assert!(result.is_decode());
     }
 }
