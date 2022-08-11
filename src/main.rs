dafcfc69851049a1c04fd780ff3a836211f432f9
Rusty Pickle <rusty.pickle94@gmail.com>
2022-08-11 22:52:41 +0600

    Update

--- src/main.rs
+++ src/main.rs
@@ -8,3 +8,12 @@ fn main() {
     }
     rex::initializer(is_windows, verifying_path).unwrap();
 }
+
+// new release targets
+// [ ] Transfer balance between methods and add transaction with amount 0 showing changes
+// [ ] yearly chart showing balance changes
+// [ ] maintain the ID num of the db when editing transaction
+// [ ] konsole error.txt being created
+// [ ] do simple calculation on amount field
+// [ ] move popup to an enum and then cu_tab(enum) to check
+// [ ] move key checker to a different file
