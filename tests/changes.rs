8c8202e25f66945cb44e832730873b518ce00656
Rusty Pickle <rusty.pickle94@gmail.com>
2022-09-10 23:43:09 +0600

    Update Tests

---
+++ tests/changes.rs
@@ -0,0 +1,104 @@
+extern crate rex;
+use rex::db::*;
+use rusqlite::{Connection};
+use std::fs;
+//use std::collections::HashMap;
+
+fn create_test_db(file_name: &str) -> Connection {
+    create_db(file_name, vec!["test1".to_string(), "test 2".to_string()]).unwrap();
+    return Connection::open(file_name).unwrap();
+}
+
+#[test]
+fn check_empty_changes() {
+    let file_name = "empty_changes.sqlite";
+    let conn = create_test_db(file_name);
+    let data = get_empty_changes(&conn);
+    conn.close().unwrap();
+
+    fs::remove_file(file_name).unwrap();
+
+    assert_eq!(
+        data,
+        vec![
+            "Changes".to_string(),
+            "0.00".to_string(),
+            "0.00".to_string()
+        ]
+    );
+}
+
+#[test]
+fn check_getting_all_changes() {
+    let file_name = "getting_changes_1.sqlite";
+    let conn = create_test_db(file_name);
+    let data = get_all_changes(&conn, 5, 6);
+    let empty_data: Vec<Vec<String>> = Vec::new();
+
+    conn.close().unwrap();
+    fs::remove_file(file_name).unwrap();
+
+    assert_eq!(data, empty_data);
+}
+
+#[test]
+fn check_getting_all_changes_2() {
+    let file_name = "getting_changes_2.sqlite".to_string();
+    let conn = create_test_db(&file_name);
+
+    add_new_tx(
+        "2022-07-19",
+        "Testing transaction",
+        "test1",
+        "159.00",
+        "Expense",
+        &file_name,
+        None,
+    )
+    .unwrap();
+
+    add_new_tx(
+        "2022-07-19",
+        "Testing transaction",
+        "test 2",
+        "159.00",
+        "Expense",
+        &file_name,
+        None,
+    )
+    .unwrap();
+
+    add_new_tx(
+        "2022-05-01",
+        "Testing transaction",
+        "test 2",
+        "753.00",
+        "Expense",
+        &file_name,
+        None,
+    )
+    .unwrap();
+
+    // This is the index of the interface. year 0 = 2022, month 0 = January
+    let data_1 = get_all_changes(&conn, 6, 0);
+    let expected_data_1: Vec<Vec<String>> = vec![
+        vec!["↓159.00".to_string(), "0.00".to_string()],
+        vec!["0.00".to_string(), "↓159.00".to_string()],
+    ];
+
+    let another_data = get_all_changes(&conn, 4, 0);
+
+    let another_expected = vec![vec!["0.00".to_string(), "↓753.00".to_string()]];
+
+    delete_tx(2, &file_name).unwrap();
+
+    let data_2 = get_all_changes(&conn, 6, 0);
+    let expected_data_2: Vec<Vec<String>> = vec![vec!["↓159.00".to_string(), "0.00".to_string()]];
+
+    conn.close().unwrap();
+    fs::remove_file(file_name).unwrap();
+
+    assert_eq!(data_1, expected_data_1);
+    assert_eq!(data_2, expected_data_2);
+    assert_eq!(another_data, another_expected);
+}
---
+++ tests/others.rs
@@ -0,0 +1,117 @@
+extern crate rex;
+use rex::db::*;
+//use rusqlite::{Connection};
+//use std::fs;
+//use std::collections::HashMap;
+
+//fn create_test_db(file_name: &str) -> Connection {
+//    create_db(file_name, vec!["test1".to_string(), "test 2".to_string()]).unwrap();
+//    return Connection::open(file_name).unwrap();
+//}
+
+struct Testing {
+    data: String
+}
+impl StatusChecker for Testing {}
+
+#[test]
+fn check_sql_dates() {
+    let data = get_sql_dates(11, 2);
+    let expected_data = ("2024-11-01".to_string(), "2024-11-31".to_string());
+    assert_eq!(data, expected_data);
+}
+
+#[test]
+fn check_verifier_date_1() {
+    let test_struct = Testing { data: "".to_string()};
+    let mut to_verify =  test_struct.data.clone();
+
+    let result = test_struct.verify_date(&mut to_verify).unwrap();
+    let expected_data = "Date: Nothing to check".to_string();
+    assert_eq!(result, expected_data);
+
+    let test_struct = Testing { data: "01-01-2022".to_string()};
+    let mut to_verify =  test_struct.data.clone();
+
+    let result = test_struct.verify_date(&mut to_verify).unwrap();
+    let expected_data = "Date: Year length not acceptable. Example Date: 2022-05-01".to_string();
+    assert_eq!(result, expected_data);
+
+    let test_struct = Testing { data: "2022-01-01".to_string()};
+    let mut to_verify =  test_struct.data.clone();
+
+    let result = test_struct.verify_date(&mut to_verify).unwrap();
+    let expected_data = "Date: Date Accepted".to_string();
+    assert_eq!(result, expected_data);
+
+    let test_struct = Testing { data: "2022-01".to_string()};
+    let mut to_verify =  test_struct.data.clone();
+
+    let result = test_struct.verify_date(&mut to_verify).unwrap();
+    let expected_data = "Date: Unknown date".to_string();
+    assert_eq!(result, expected_data);
+}
+
+#[test]
+fn check_verifier_date_2() {
+    let test_struct = Testing { data: "2022-01-0".to_string()};
+    let mut to_verify =  test_struct.data.clone();
+
+    let result = test_struct.verify_date(&mut to_verify).unwrap();
+    let expected_data = "Date: Day length not acceptable. Example Date: 2022-05-01".to_string();
+    assert_eq!(result, expected_data);
+
+    let test_struct = Testing { data: "2022-0-01".to_string()};
+    let mut to_verify =  test_struct.data.clone();
+
+    let result = test_struct.verify_date(&mut to_verify).unwrap();
+    let expected_data = "Date: Month length not acceptable. Example Date: 2022-05-01".to_string();
+    assert_eq!(result, expected_data);
+
+    let test_struct = Testing { data: "202-01-01".to_string()};
+    let mut to_verify =  test_struct.data.clone();
+
+    let result = test_struct.verify_date(&mut to_verify).unwrap();
+    let expected_data = "Date: Year length not acceptable. Example Date: 2022-05-01".to_string();
+    assert_eq!(result, expected_data);
+}
+
+#[test]
+fn check_verifier_date_3() {
+    let test_struct = Testing { data: "2022-01-32".to_string()};
+    let mut to_verify =  test_struct.data.clone();
+
+    let result = test_struct.verify_date(&mut to_verify).unwrap();
+    let expected_data = "Date: Day must be between 01-31".to_string();
+    assert_eq!(result, expected_data);
+
+    let test_struct = Testing { data: "2022-02-31".to_string()};
+    let mut to_verify =  test_struct.data.clone();
+
+    let result = test_struct.verify_date(&mut to_verify).unwrap();
+    let expected_data = "Date: Date not acceptable and possibly non-existing. Error: input is out of range".to_string();
+    assert_eq!(result, expected_data);
+
+    let test_struct = Testing { data: "2022-13-31".to_string()};
+    let mut to_verify =  test_struct.data.clone();
+
+    let result = test_struct.verify_date(&mut to_verify).unwrap();
+    let expected_data = "Date: Month must be between 01-12".to_string();
+    assert_eq!(result, expected_data);
+
+    let test_struct = Testing { data: "2026-01-31".to_string()};
+    let mut to_verify =  test_struct.data.clone();
+
+    let result = test_struct.verify_date(&mut to_verify).unwrap();
+    let expected_data = "Date: Year must be between 2022-2025".to_string();
+    assert_eq!(result, expected_data);
+}
+
+#[test]
+#[should_panic]
+fn check_verifier_date_4() {
+    let test_struct = Testing { data: "2022-01-".to_string()};
+    let mut to_verify =  test_struct.data.clone();
+
+    test_struct.verify_date(&mut to_verify).unwrap();
+}
