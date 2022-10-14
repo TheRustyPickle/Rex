2a4ac948136af1a9a8776f57918f1f30c36b1920
Rusty Pickle <rusty.pickle94@gmail.com>
2022-10-14 22:58:23 +0600

    Added summary key tracker

---
+++ src/key_checker/summary_keys.rs
@@ -0,0 +1,45 @@
+use crate::home_page::{CurrentUi, PopupState, TableData, TxTab};
+use crate::tx_page::AddTxData;
+use crossterm::event::{KeyCode, KeyEvent};
+use std::error::Error;
+
+pub fn summary_keys(
+    key: KeyEvent,
+    cu_page: &mut CurrentUi,
+    cu_popup: &mut PopupState,
+    cu_tx_page: &mut TxTab,
+    data_for_tx: &mut AddTxData,
+    summary_table: &mut TableData,
+    total_index: usize,
+) -> Result<String, Box<dyn Error>> {
+    match cu_popup {
+        PopupState::Nothing => match key.code {
+            KeyCode::Char('q') => return Ok("".to_string()),
+            KeyCode::Char('f') => {
+                // * returns to home page and reloads data
+                *cu_page = CurrentUi::Home;
+                *cu_tx_page = TxTab::Nothing;
+                *data_for_tx = AddTxData::new();
+            }
+            KeyCode::Char('h') => *cu_popup = PopupState::Helper,
+            KeyCode::Up => {
+                if summary_table.state.selected() == Some(0) {
+                    summary_table.state.select(Some(total_index - 1));
+                } else {
+                    summary_table.previous();
+                }
+            }
+            KeyCode::Down => {
+                if summary_table.state.selected() == Some(total_index - 1) {
+                    summary_table.state.select(Some(0));
+                } else {
+                    summary_table.next();
+                }
+            }
+            _ => {}
+        },
+        _ => {}
+    }
+
+    Ok("0".to_string())
+}
