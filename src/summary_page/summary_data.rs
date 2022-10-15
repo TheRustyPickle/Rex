0415666e015cce4f0a273f15429ffea4b6893b20
Rusty Pickle <rusty.pickle94@gmail.com>
2022-10-16 02:52:47 +0600

    Summary data handler

--- src/summary_page/summary_data.rs
+++ src/summary_page/summary_data.rs
@@ -1,18 +1,20 @@
 use crate::db::{get_all_txs, get_month_name};
 use rusqlite::Connection;
 use std::collections::HashMap;
-
 /// Contains the necessary information to construct the Summary Page highlighting
 /// tag based expense and income information, biggest expense and income
 
 // TODO add some tests for this struct
+// TODO add comments
 pub struct SummaryData {
     tags_income: HashMap<String, f64>,
     tags_expense: HashMap<String, f64>,
     biggest_income: (f64, String),
     biggest_expense: (f64, String),
-    income_date: (f64, String, String),
-    expense_date: (f64, String, String),
+    income_date: (f64, String),
+    expense_date: (f64, String),
+    total_income: f64,
+    total_expense: f64,
 }
 
 impl SummaryData {
@@ -20,10 +22,12 @@ impl SummaryData {
         let mut default = SummaryData {
             tags_income: HashMap::new(),
             tags_expense: HashMap::new(),
-            biggest_income: (0.0, String::new()),
-            biggest_expense: (0.0, String::new()),
-            income_date: (0.0, String::new(), String::new()),
-            expense_date: (0.0, String::new(), String::new()),
+            biggest_income: (0.0, String::from("Not Found")),
+            biggest_expense: (0.0, String::from("Not Found")),
+            income_date: (0.0, String::from("Not Found")),
+            expense_date: (0.0, String::from("Not Found")),
+            total_income: 0.0,
+            total_expense: 0.0,
         };
 
         for year in 0..4 {
@@ -31,7 +35,6 @@ impl SummaryData {
                 default.collect_data(conn, month, year);
             }
         }
-        
 
         default
     }
@@ -40,7 +43,7 @@ impl SummaryData {
         let mut to_return = Vec::new();
 
         for (key, value) in &self.tags_income {
-            let mut to_push = vec![key.to_string(), format!("{:.2}",value)];
+            let mut to_push = vec![key.to_string(), format!("{:.2}", value)];
             if self.tags_expense.contains_key(key) {
                 to_push.push(self.tags_expense[key].to_string());
             } else {
@@ -51,23 +54,36 @@ impl SummaryData {
 
         for (key, value) in &self.tags_expense {
             if !self.tags_income.contains_key(key) {
-                to_return.push(vec![key.to_string(), "0".to_string(), format!("{:.2}",value)])
+                to_return.push(vec![
+                    key.to_string(),
+                    "0".to_string(),
+                    format!("{:.2}", value),
+                ])
             }
         }
         to_return
+    }
+
+    pub fn get_tx_data(&self) -> Vec<(f64, String)> {
+        vec![
+            (self.total_income, "Total Income:".to_string()),
+            (self.total_expense, "Total Expense:".to_string()),
+            self.biggest_income.to_owned(),
+            self.biggest_expense.to_owned(),
+            self.income_date.to_owned(),
+            self.expense_date.to_owned(),
+        ]
     }
 
     fn collect_data(&mut self, conn: &Connection, month: usize, year: usize) {
         let mut total_income = 0.0;
         let mut total_expense = 0.0;
 
-        let mut biggest_income = (0.0, "".to_string());
-        let mut biggest_expense = (0.0, "".to_string());
-
         let (cu_month, cu_year) = get_month_name(month, year);
 
         let (all_tx, ..) = get_all_txs(conn, month, year);
         for tx in all_tx {
+            let tx_date = &tx[0];
             let tx_method = &tx[2];
             let tx_amount: f64 = tx[3].parse().unwrap();
             let tx_type = &tx[4];
@@ -75,9 +91,9 @@ impl SummaryData {
 
             if tx_type == "Income" {
                 if tx_amount > self.biggest_income.0 {
-                    self.biggest_income = (tx_amount, tx_method.to_string());
-                    total_income += tx_amount;
+                    self.biggest_income = (tx_amount, format!("{}, Date: {}", tx_method, tx_date));
                 }
+                total_income += tx_amount;
 
                 for tag in tx_tags {
                     if self.tags_income.contains_key(tag) {
@@ -87,15 +103,11 @@ impl SummaryData {
                         self.tags_income.insert(tag.to_string(), tx_amount);
                     }
                 }
-
-                if tx_amount > biggest_income.0 {
-                    biggest_income = (tx_amount, tx_method.to_string());
-                }
             } else if tx_type == "Expense" {
                 if tx_amount > self.biggest_expense.0 {
-                    self.biggest_expense = (tx_amount, tx_method.to_string());
-                    total_expense += tx_amount;
+                    self.biggest_expense = (tx_amount, format!("{}, Date: {}", tx_method, tx_date));
                 }
+                total_expense += tx_amount;
 
                 for tag in tx_tags {
                     if self.tags_expense.contains_key(tag) {
@@ -105,26 +117,17 @@ impl SummaryData {
                         self.tags_expense.insert(tag.to_string(), tx_amount);
                     }
                 }
-
-                if tx_amount > biggest_expense.0 {
-                    biggest_expense = (tx_amount, tx_method.to_string());
-                }
             }
         }
 
         if total_income > self.income_date.0 {
-            self.income_date = (total_income, cu_month.clone(), cu_year.clone());
+            self.income_date = (total_income, format!("{} of {}", cu_month, cu_year));
         }
         if total_expense > self.expense_date.0 {
-            self.expense_date = (total_expense, cu_month, cu_year);
+            self.expense_date = (total_expense, format!("{} of {}", cu_month, cu_year));
         }
 
-        if self.biggest_income.0 > biggest_income.0 {
-            self.biggest_income = biggest_income
-        }
-
-        if self.biggest_expense.0 > biggest_expense.0 {
-            self.biggest_expense = biggest_expense
-        }
+        self.total_income = total_income + self.total_income;
+        self.total_expense = total_expense + self.total_expense;
     }
 }
