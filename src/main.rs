e2119f0dbd0c581575cb3531cb07c0543b03bc22
Rusty Pickle <rusty.pickle94@gmail.com>
2022-10-20 22:20:32 +0600

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
+use dirs::data_local_dir;
+use std::env::set_current_dir;
+use std::fs;
 fn main() {
-    let mut is_windows = false;
-    let mut verifying_path = "./data.sqlite";
-    // change details if running on windows
-    if cfg!(target_os = "windows") {
-        is_windows = true;
-        verifying_path = r#".\data.sqlite"#;
+    if let Some(dir) = data_local_dir() {
+        let mut is_windows = false;
+        let mut verifying_path = "./data.sqlite";
+
+        // * OS based path where data will be stored
+        let working_path = format!("{}/Rex/", dir.display());
+
+        if cfg!(target_os = "windows") {
+            is_windows = true;
+            verifying_path = r#".\data.sqlite"#;
+        }
+        // * Create folder if non-existing then move the current working directory
+        // * to the OS data directory
+        fs::create_dir_all(&working_path).unwrap();
+        set_current_dir(working_path).unwrap();
+        rex::initializer(is_windows, &verifying_path).unwrap();
+    } else {
+        println!("Could not find local data directory. Exiting program...");
     }
-    rex::initializer(is_windows, verifying_path).unwrap();
 }
 
 // [x] consider b as current balance when doing calculation in the UI
@@ -20,4 +34,4 @@ fn main() {
 // [x] edit add tx and transfer page help text
 // [x] add this ALTER TABLE tx_all ADD tags TEXT DEFAULT Unknown;
 // [x] maintain tx method order during db creation
-// TODO find how to use cargo install
+// [x] find how to use cargo install. dirs crate
