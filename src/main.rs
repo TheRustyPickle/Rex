63abc2b3cdafe685d47a6cf6557f63e153c401e7
WaffleMixer <mohammad.sakib0194@gmail.com>
2022-10-23 22:50:58 +0600

    Fix directory issue when opening terminal

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use dirs::data_local_dir;
-use std::env::set_current_dir;
+use std::env::{set_current_dir, current_dir};
 use std::fs;
 fn main() {
     if let Some(dir) = data_local_dir() {
         let mut is_windows = false;
+        let current_dir = current_dir().unwrap().display().to_string();
         let mut verifying_path = "./data.sqlite";
 
         // * OS based path where data will be stored
@@ -17,7 +18,7 @@ fn main() {
         // * to the OS data directory
         fs::create_dir_all(&working_path).unwrap();
         set_current_dir(working_path).unwrap();
-        rex::initializer(is_windows, &verifying_path).unwrap();
+        rex::initializer(is_windows, &verifying_path, &current_dir).unwrap();
     } else {
         println!("Could not find local data directory. Exiting program...");
     }
