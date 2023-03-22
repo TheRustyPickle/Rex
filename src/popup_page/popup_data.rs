628b81d4a5f7aede2837d0c98d662aa10e3efaa2
Rusty Pickle <rusty.pickle94@gmail.com>
2023-03-23 02:23:30 +0600

    Update help text

--- src/popup_page/popup_data.rs
+++ src/popup_page/popup_data.rs
@@ -3,7 +3,7 @@ use crate::popup_page::create_popup;
 use tui::{backend::Backend, Frame};
 
 // TODO turn data into a struct for easier access
-
+// TODO give specific help page for each tab?
 pub fn add_popup<B: Backend>(f: &mut Frame<B>, popup_type: &PopupState) {
     let mut data = Vec::new();
 
@@ -21,27 +21,27 @@ pub fn add_popup<B: Backend>(f: &mut Frame<B>, popup_type: &PopupState) {
         PopupState::Helper => {
             data.push("Help".to_string());
             data.push(
-                "'Arrow Key' : Navigate
-'A' : Add Transaction Page
-'T' : Add Transfer Page
-'R' : Balance Chart (Follows your selected year)
-'Z' : Get Transaction Summary
-'F' : Home Page
-'D' : Delete selected Transaction (Home Page)
-'J' : Add new Transaction Methods (Home Page)
-'E' : Edit Selected Transaction (Home Page)
-'H' : Open Hotkey Help
-'Q' : Quit
-
+                "Arrow Key : Navigate
+A: Add Transaction Page
+T: Add Transfer Page
+R: Balance Chart (Follows your selected year)
+Z: Get Transaction Summary
+F: Home Page
+D: Delete selected Transaction (Home Page)
+J: Add new Transaction Methods (Home Page)
+E: Edit Selected Transaction (Home Page)
+H: Open Hotkey Help
+Q: Quit
+                
 Add Transaction/Transfer Page:
-'1' : Edit Date           '4' : Edit Amount/To Method
-'2' : Edit TX details     '5' : Edit TX Type/Amount
-'3' : Edit TX/From Method '6' : Edit Tags
+1: Edit Date           4: Edit Amount/To Method
+2: Edit TX details     5: Edit TX Type/Amount
+3: Edit TX/From Method 6: Edit Tags  
 
-'S' : Save the data as a Transaction
-'Enter' : Submit field and continue
-'Esc' : Stop editing filed\n
-Press Any Key to dismiss"
+S: Save inputted data as a Transaction
+Enter: Submit a field and continue
+Esc: Stop editing a filed\n
+Press Any Key To Dismiss"
                     .to_string(),
             );
             data.push("50".to_string());
