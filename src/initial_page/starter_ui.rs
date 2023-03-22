d046a50b7aaa0f8e97963e819011284a91c1a6d1
Rusty Pickle <mohammad.sakib0194@gmail.com>
2023-03-23 02:23:30 +0600

    Update help text

--- src/initial_page/starter_ui.rs
+++ src/initial_page/starter_ui.rs
@@ -10,7 +10,7 @@ use tui::{
 /// The initial UI that starts on the startup of the program. The function
 /// draws 2 widgets with the intention to show the hotkeys of the program.
 /// Takes an additional vector parameter to show pop up if necessary.
-pub fn starter_ui<B: Backend>(f: &mut Frame<B>, index: usize) {
+pub fn starter_ui<B: Backend>(f: &mut Frame<B>, start_from: usize) {
     let size = f.size();
     let chunks = Layout::default()
         .direction(Direction::Vertical)
@@ -39,87 +39,78 @@ pub fn starter_ui<B: Backend>(f: &mut Frame<B>, index: usize) {
     f.render_widget(block, size);
 
     // This is the text that is shown in the startup which is the project's name in ASCII format.
-    let text = r#"  ____    _____  __  __
-    |  _ \  | ____| \ \/ /
-    | |_) | |  _|    \  / 
-    |  _ <  | |___   /  \ 
-    |_| \_\ |_____| /_/\_\
-                          "#
-    .to_string();
+    let text = r#"   _____    ______  __   __
+  |  __ \  |  ____| \ \ / /
+  | |__) | | |__     \ V / 
+  |  _  /  |  __|     > <  
+  | | \ \  | |____   / . \ 
+  |_|  \_\ |______| /_/ \_\"#
+        .to_string();
 
     // To work with this and add a slight touch of animation, we will split the entire
-    // text by \n. Once it is done, we will loop through each line and add the chars in a string for rendering.
+    // text by \n. Once it is done, we will loop through each line and take a specific amount of chars from each line.
     let state = text.split('\n');
     let splitted = state.collect::<Vec<&str>>();
-    let mut new_text = String::new();
+    let mut upper_text = String::new();
 
     for line in splitted {
-        // Let's take a look what each variable is does:
-        // total_initial_to_add : Once the index goes to the end, we want to already start rendering
-        // from the beginning so this variable contains the amount of chars from the beginning of text to add for rendering.
-        let mut total_initial_to_add = 0;
-
-        // total_to_add : This is the amount of chars to added for rendering each loop
+        // if the line is 20 chars and the index is 15, take the chars from 15-20 and 0-3 indexes.
+        // this var stores how many to take from 0 index
+        let mut to_add_from_start = 0;
+        // amount of chars per line
         let mut total_to_add = 10;
 
-        // This is where the chars selection from the start begins once the index goes above the text's
-        // length. -1 is added because we are working with index which starts at 0.
-        if index + total_to_add > line.len() {
-            total_initial_to_add = index + total_to_add - 1 - line.len();
-
-            // unsure why this part works but it makes the rendering a bit smoother.
-            if total_initial_to_add > total_to_add - 1 {
-                total_initial_to_add = total_to_add - 1
-            }
+        if start_from + total_to_add > line.len() {
+            let extra_index = (start_from + total_to_add) - line.len();
+            // add extra index to take from beginning and remove from the starting point
+            // if it will go out of bound
+            total_to_add -= extra_index;
+            to_add_from_start += extra_index;
         }
 
-        // after each loop we keep track of the loop index and the text index we want to add for rendering.
-        let mut cu_index = 0;
-        let mut target_index = index;
-
-        for char in line.chars() {
-            if cu_index == target_index && total_to_add != 0 && target_index < line.len() {
-                new_text.push(char);
+        // go through each char of the line
+        for (index, char) in line.chars().enumerate() {
+            if to_add_from_start != 0 {
+                // add chars if we have to take anything from index 0 to something index
+                upper_text.push(char);
+                to_add_from_start -= 1;
+            } else if total_to_add != 0 && index >= start_from {
+                // if we are at the start point, take the char
+                upper_text.push(char);
                 total_to_add -= 1;
-                cu_index += 1;
-                target_index += 1
-            } else if total_initial_to_add != 0 {
-                new_text.push(char);
-                cu_index += 1;
-                total_initial_to_add -= 1;
-            } else {
-                new_text.push(' ');
-                cu_index += 1;
+            } else if index != start_from || total_to_add == 0 {
+                // if the 10 char limit is crossed, only add empty space
+                upper_text.push(' ');
             }
         }
-        new_text.push('\n');
+        upper_text.push('\n');
     }
+    upper_text.push_str("\n  Press Any Key To Continue");
 
-    new_text.push_str("\n    Press Any Key To Continue");
-
-    let second_text = "'Arrow Key' : Navigate
-'A' : Add Transaction Page
-'T' : Add Transfer Page
-'R' : Balance Chart (Follows your selected year)
-'Z' : Get Transaction Summary
-'F' : Home Page
-'D' : Delete selected Transaction (Home Page)
-'J' : Add new Transaction Methods (Home Page)
-'E' : Edit Selected Transaction (Home Page)
-'H' : Open Hotkey Help
-'Q' : Quit
+    // TODO add more informative stuff here
+    let second_text = "Arrow Key : Navigate
+A: Add Transaction Page
+T: Add Transfer Page
+R: Balance Chart (Follows your selected year)
+Z: Get Transaction Summary
+F: Home Page
+D: Delete selected Transaction (Home Page)
+J: Add new Transaction Methods (Home Page)
+E: Edit Selected Transaction (Home Page)
+H: Open Hotkey Help
+Q: Quit
 
 Add Transaction/Transfer Page:
-'1' : Edit Date           '4' : Edit Amount/To Method
-'2' : Edit TX details     '5' : Edit TX Type/Amount
-'3' : Edit TX/From Method '6' : Edit Tags  
+1: Edit Date           4: Edit Amount/To Method
+2: Edit TX details     5: Edit TX Type/Amount
+3: Edit TX/From Method 6: Edit Tags  
 
-'S' : Save the data as a Transaction
-'Enter' : Submit field and continue
-'Esc' : Stop editing filed
+S: Save inputted data as a Transaction
+Enter: Submit a field and continue
+Esc: Stop editing a filed
 ";
 
-    let paragraph = Paragraph::new(new_text)
+    let paragraph = Paragraph::new(upper_text)
         .style(
             Style::default()
                 .bg(Color::Rgb(255, 255, 255))
