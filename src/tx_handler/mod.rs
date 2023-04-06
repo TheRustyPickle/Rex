8caff4643c42684d44441d991d25f5045a751a9f
Rusty Pickle <rusty.pickle94@gmail.com>
2023-04-07 05:45:25 +0600

    mod updates

--- src/tx_handler/mod.rs
+++ src/tx_handler/mod.rs
@@ -1,5 +1,9 @@
 mod add_tx;
 mod delete_tx;
+mod tx_data;
+mod verifier;
 
 pub use add_tx::add_tx;
 pub use delete_tx::delete_tx;
+pub use tx_data::*;
+pub use verifier::*;
--- src/utility/mod.rs
+++ src/utility/mod.rs
@@ -1,7 +1,5 @@
 mod sub_func;
-mod verifier;
 mod utils;
 
 pub use sub_func::*;
-pub use verifier::*;
 pub use utils::*;
