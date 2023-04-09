225bc2087c554e7245d1049b00ba36d6e1c22ef6
Rusty Pickle <mohammad.sakib0194@gmail.com>
2023-04-09 06:05:44 +0600

    added new key for pages

--- src/key_checker/chart_keys.rs
+++ src/key_checker/chart_keys.rs
@@ -4,11 +4,17 @@ use crate::ui_handler::PopupState;
 use crossterm::event::KeyCode;
 
 pub fn chart_keys(handler: &mut InputKeyHandler) -> Option<HandlingOutput> {
-    match handler.current_popup {
+    match handler.popup {
         PopupState::Nothing => match handler.key.code {
+            KeyCode::Char('a') => handler.go_add_tx(),
+            KeyCode::Char('t') => handler.go_transfer(),
             KeyCode::Char('q') => return Some(HandlingOutput::QuitUi),
             KeyCode::Char('f') => handler.go_home(),
             KeyCode::Char('h') => handler.do_help_popup(),
+            KeyCode::Right => handler.handle_right_arrow(),
+            KeyCode::Left => handler.handle_left_arrow(),
+            KeyCode::Up => handler.handle_up_arrow(),
+            KeyCode::Down => handler.handle_down_arrow(),
             _ => {}
         },
         _ => handler.do_empty_popup(),
