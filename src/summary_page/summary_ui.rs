5daff5ed0e33ea0eae3893a25ecf30815d0dabb6
Rusty Pickle <rusty.pickle94@gmail.com>
2023-06-11 05:04:35 +0600

    Formatting

--- src/summary_page/summary_ui.rs
+++ src/summary_page/summary_ui.rs
@@ -3,13 +3,13 @@ use crate::page_handler::{
 };
 use crate::summary_page::SummaryData;
 use crate::utility::{create_tab, get_all_tx_methods, main_block, styled_block};
-use rusqlite::Connection;
-use thousands::Separable;
 use ratatui::backend::Backend;
 use ratatui::layout::{Constraint, Direction, Layout};
 use ratatui::style::{Modifier, Style};
 use ratatui::widgets::{Cell, Row, Table};
 use ratatui::Frame;
+use rusqlite::Connection;
+use thousands::Separable;
 
 /// The function draws the Summary page of the interface.
 #[cfg(not(tarpaulin_include))]
