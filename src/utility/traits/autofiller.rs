bca27b50e1381ddef5531616af37ffa1bffc13ae
Rusty Pickle <rusty.pickle94@gmail.com>
2023-06-12 05:44:25 +0600

    Added autofiller trait

---
+++ src/utility/traits/autofiller.rs
@@ -0,0 +1,46 @@
+use crate::utility::{get_all_tags, get_all_tx_methods, get_best_match};
+use rusqlite::Connection;
+
+pub trait AutoFiller {
+    fn autofill_tx_method(&self, user_input: &str, conn: &Connection) -> String {
+        if !user_input.is_empty() {
+            let all_tx_methods = get_all_tx_methods(conn);
+            let best_match = get_best_match(user_input, all_tx_methods);
+
+            if best_match == user_input {
+                String::new()
+            } else {
+                best_match
+            }
+        } else {
+            String::new()
+        }
+    }
+
+    fn autofill_tags(&self, user_input: &str, conn: &Connection) -> String {
+        let all_tags = get_all_tags(conn);
+
+        if !user_input.is_empty() && !all_tags.is_empty() {
+            let splitted = user_input
+                .split(',')
+                .map(|s| s.trim())
+                .collect::<Vec<&str>>();
+
+            let last_value = splitted.last().unwrap();
+
+            if last_value.is_empty() {
+                return String::new();
+            }
+
+            let best_match = get_best_match(last_value, all_tags);
+
+            if &best_match == last_value {
+                String::new()
+            } else {
+                best_match
+            }
+        } else {
+            String::new()
+        }
+    }
+}
