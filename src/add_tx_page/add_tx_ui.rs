1d2aa31a263457c76ddbbb080e2edf39af4c5fa2
Rusty Pickle <rusty.pickle94@gmail.com>
2023-06-24 03:29:38 +0600

    Start upgrading add tx ui to handle transfer

--- src/add_tx_page/add_tx_ui.rs
+++ src/add_tx_page/add_tx_ui.rs
@@ -1,6 +1,7 @@
+use crate::outputs::TxType;
 use crate::page_handler::{TxTab, BACKGROUND, BLUE, GRAY, RED, TEXT};
 use crate::tx_handler::TxData;
-use crate::utility::{create_bolded_text, main_block, styled_block};
+use crate::utility::{main_block, styled_block};
 use ratatui::backend::Backend;
 use ratatui::layout::{Alignment, Constraint, Direction, Layout};
 use ratatui::style::{Modifier, Style};
@@ -10,10 +11,11 @@ use ratatui::Frame;
 
 /// The function draws the Add Transaction page of the interface.
 #[cfg(not(tarpaulin_include))]
-pub fn add_tx_ui<B: Backend>(f: &mut Frame<B>, add_tx_data: &TxData, currently_selected: &TxTab) {
+pub fn add_tx_ui<B: Backend>(f: &mut Frame<B>, add_tx_data: &TxData, add_tx_tab: &TxTab) {
     // get the data to insert into the Status widget of this page
+
     let status_data = add_tx_data.get_tx_status();
-    // * Contains date, details, from method, to method, amount, tx type, tags.
+    // Contains date, details, from method, to method, amount, tx type, tags.
     // Except to method, rest will be used for the widgets
     let input_data = add_tx_data.get_all_texts();
     // The index of the cursor position
@@ -21,14 +23,19 @@ pub fn add_tx_ui<B: Backend>(f: &mut Frame<B>, add_tx_data: &TxData, currently_s
 
     let size = f.size();
 
-    // divide the terminal into 4 parts vertically
+    let tx_type = add_tx_data.get_tx_type();
+
+    let from_method_name = match tx_type {
+        TxType::IncomeExpense => "TX Method",
+        TxType::Transfer => "From Method",
+    };
+
+    // divide the terminal into 3 parts vertically
     let chunks = Layout::default()
         .direction(Direction::Vertical)
         .margin(2)
         .constraints(
             [
-                // helper chunk
-                Constraint::Length(13),
                 // input chunk
                 Constraint::Length(3),
                 // details input chunk
@@ -40,40 +47,42 @@ pub fn add_tx_ui<B: Backend>(f: &mut Frame<B>, add_tx_data: &TxData, currently_s
         )
         .split(size);
 
-    // divide the second chunk into 5 parts horizontally
+    // based on the tx type divide the first chunk into 5 or 6 parts horizontally
     // this chunk contains the input boxes take takes input
-    let input_chunk = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints(
-            [
-                Constraint::Percentage(20),
-                Constraint::Percentage(20),
-                Constraint::Percentage(20),
-                Constraint::Percentage(20),
-                Constraint::Percentage(20),
-            ]
-            .as_ref(),
-        )
-        .split(chunks[1]);
+    let input_chunk = {
+        match tx_type {
+            TxType::IncomeExpense => Layout::default()
+                .direction(Direction::Horizontal)
+                .constraints(
+                    [
+                        Constraint::Percentage(20),
+                        Constraint::Percentage(20),
+                        Constraint::Percentage(20),
+                        Constraint::Percentage(20),
+                        Constraint::Percentage(20),
+                    ]
+                    .as_ref(),
+                )
+                .split(chunks[0]),
+            TxType::Transfer => Layout::default()
+                .direction(Direction::Horizontal)
+                .constraints(
+                    [
+                        Constraint::Percentage(16),
+                        Constraint::Percentage(16),
+                        Constraint::Percentage(16),
+                        Constraint::Percentage(16),
+                        Constraint::Percentage(16),
+                        Constraint::Percentage(16),
+                    ]
+                    .as_ref(),
+                )
+                .split(chunks[0]),
+        }
+    };
 
     // creates border around the entire terminal
     f.render_widget(main_block(), size);
-
-    // This is the details of the Help widget
-    let unmodified_help_text = "Press the respective keys to edit fields.
-1: Date         Example: 2022-05-12, YYYY-MM-DD
-2: TX details   Example: For Grocery, Salary
-3: TX Method    Example: Cash, Bank, Card
-4: Amount       Example: 1000, 100+50, b - 100
-5: TX Type      Example: Income/Expense/I/E
-6: TX Tags      Example: Empty, Food, Car. Add a Comma for a new tag
-S: Save the inputted data as a Transaction
-H: Shows further detailed help info
-Enter: Submit field and continue
-Esc: Stop editing field";
-
-    // highlight texts before the first color to Bold
-    let help_text = create_bolded_text(unmodified_help_text);
 
     let mut status_text = vec![];
 
@@ -105,7 +114,9 @@ Esc: Stop editing field";
 
     let details_text = Line::from(format!("{} ", input_data[1]));
 
-    let mut tx_method_text = Line::from(format!("{} ", input_data[2]));
+    let mut from_method_text = Line::from(format!("{} ", input_data[2]));
+
+    let mut to_method_text = Line::from(format!("{} ", input_data[3]));
 
     let amount_text = Line::from(format!("{} ", input_data[4]));
 
@@ -113,10 +124,16 @@ Esc: Stop editing field";
 
     let mut tags_text = Line::from(format!("{} ", input_data[6]));
 
-    match currently_selected {
+    match add_tx_tab {
         TxTab::FromMethod => {
-            tx_method_text = Line::from(vec![
+            from_method_text = Line::from(vec![
                 Span::from(format!("{} ", input_data[2])),
+                Span::styled(input_data[7], Style::default().fg(GRAY)),
+            ]);
+        }
+        TxTab::ToMethod => {
+            to_method_text = Line::from(vec![
+                Span::from(format!("{} ", input_data[3])),
                 Span::styled(input_data[7], Style::default().fg(GRAY)),
             ]);
         }
@@ -129,12 +146,6 @@ Esc: Stop editing field";
         _ => {}
     }
 
-    // creates the widgets to ready it for rendering
-    let help_sec = Paragraph::new(help_text)
-        .style(Style::default().bg(BACKGROUND).fg(TEXT))
-        .block(styled_block("Help"))
-        .alignment(Alignment::Left);
-
     let status_sec = Paragraph::new(status_text)
         .style(Style::default().bg(BACKGROUND).fg(TEXT))
         .block(styled_block("Status"))
@@ -145,9 +156,14 @@ Esc: Stop editing field";
         .block(styled_block("Date"))
         .alignment(Alignment::Left);
 
-    let tx_method_sec = Paragraph::new(tx_method_text)
+    let from_method_sec = Paragraph::new(from_method_text)
         .style(Style::default().bg(BACKGROUND).fg(TEXT))
-        .block(styled_block("TX Method"))
+        .block(styled_block(from_method_name))
+        .alignment(Alignment::Left);
+
+    let to_method_sec = Paragraph::new(to_method_text)
+        .style(Style::default().bg(BACKGROUND).fg(TEXT))
+        .block(styled_block("To Method"))
         .alignment(Alignment::Left);
 
     let amount_sec = Paragraph::new(amount_text)
@@ -172,38 +188,69 @@ Esc: Stop editing field";
 
     // We will be adding a cursor based on which tab is selected + the selected index.
     // This was created utilizing the tui-rs example named user_input.rs
-    match currently_selected {
+    match add_tx_tab {
         TxTab::Date => f.set_cursor(
             input_chunk[0].x + current_index as u16 + 1,
             input_chunk[0].y + 1,
         ),
-        TxTab::Details => f.set_cursor(chunks[2].x + current_index as u16 + 1, chunks[2].y + 1),
-        TxTab::FromMethod => f.set_cursor(
+        TxTab::Details => f.set_cursor(chunks[1].x + current_index as u16 + 1, chunks[1].y + 1),
+        TxTab::TxType => f.set_cursor(
             input_chunk[1].x + current_index as u16 + 1,
             input_chunk[1].y + 1,
         ),
-        TxTab::Amount => f.set_cursor(
+        TxTab::FromMethod => f.set_cursor(
             input_chunk[2].x + current_index as u16 + 1,
             input_chunk[2].y + 1,
-        ),
-        TxTab::TxType => f.set_cursor(
-            input_chunk[3].x + current_index as u16 + 1,
-            input_chunk[3].y + 1,
-        ),
-        TxTab::Tags => f.set_cursor(
-            input_chunk[4].x + current_index as u16 + 1,
-            input_chunk[4].y + 1,
         ),
         _ => {}
     }
 
-    // render the previously generated data into an interface
-    f.render_widget(help_sec, chunks[0]);
-    f.render_widget(details_sec, chunks[2]);
-    f.render_widget(status_sec, chunks[3]);
+    match tx_type {
+        TxType::IncomeExpense => match add_tx_tab {
+            TxTab::Amount => f.set_cursor(
+                input_chunk[3].x + current_index as u16 + 1,
+                input_chunk[3].y + 1,
+            ),
+
+            TxTab::Tags => f.set_cursor(
+                input_chunk[4].x + current_index as u16 + 1,
+                input_chunk[4].y + 1,
+            ),
+            _ => {}
+        },
+        TxType::Transfer => match add_tx_tab {
+            TxTab::ToMethod => f.set_cursor(
+                input_chunk[3].x + current_index as u16 + 1,
+                input_chunk[3].y + 1,
+            ),
+            TxTab::Amount => f.set_cursor(
+                input_chunk[4].x + current_index as u16 + 1,
+                input_chunk[4].y + 1,
+            ),
+
+            TxTab::Tags => f.set_cursor(
+                input_chunk[5].x + current_index as u16 + 1,
+                input_chunk[5].y + 1,
+            ),
+            _ => {}
+        },
+    }
+
+    f.render_widget(details_sec, chunks[1]);
+    f.render_widget(status_sec, chunks[2]);
     f.render_widget(date_sec, input_chunk[0]);
-    f.render_widget(tx_method_sec, input_chunk[1]);
-    f.render_widget(amount_sec, input_chunk[2]);
-    f.render_widget(tx_type_sec, input_chunk[3]);
-    f.render_widget(tags_sec, input_chunk[4]);
+    f.render_widget(tx_type_sec, input_chunk[1]);
+    f.render_widget(from_method_sec, input_chunk[2]);
+
+    match tx_type {
+        TxType::IncomeExpense => {
+            f.render_widget(amount_sec, input_chunk[3]);
+            f.render_widget(tags_sec, input_chunk[4]);
+        }
+        TxType::Transfer => {
+            f.render_widget(to_method_sec, input_chunk[3]);
+            f.render_widget(amount_sec, input_chunk[4]);
+            f.render_widget(tags_sec, input_chunk[5]);
+        }
+    }
 }
