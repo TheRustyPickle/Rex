202a8a6c8dfed5dbb7baca6451a2b5f848599d4f
Rusty Pickle <mohammad.sakib0194@gmail.com>
2023-06-27 04:53:11 +0600

    Initial db querying for searching

--- src/outputs/output.rs
+++ src/outputs/output.rs
@@ -113,11 +113,23 @@ pub enum TxType {
     Transfer,
 }
 
-// TODO allow using to_string on them to use inside SQL statements
 pub enum ComparisonType {
     Equal,
     BiggerThan,
     SmallerThan,
     EqualOrBigger,
     EqualOrSmaller,
+}
+
+impl fmt::Display for ComparisonType {
+    #[cfg(not(tarpaulin_include))]
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ComparisonType::Equal => write!(f, "amount ="),
+            ComparisonType::BiggerThan => write!(f, "CAST(amount AS REAL) >"),
+            ComparisonType::SmallerThan => write!(f, "CAST(amount AS REAL) <"),
+            ComparisonType::EqualOrBigger => write!(f, "CAST(amount AS REAL) >="),
+            ComparisonType::EqualOrSmaller => write!(f, "CAST(amount AS REAL) <="),
+        }
+    }
 }
