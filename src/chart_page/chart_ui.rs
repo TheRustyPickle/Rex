84b3e00f43874ac1692981f462b82107365c28dd
Rusty Pickle <mohammad.sakib0194@gmail.com>
2024-02-23 03:14:22 +0600

    Clippy fixes

--- src/chart_page/chart_ui.rs
+++ src/chart_page/chart_ui.rs
@@ -287,7 +287,7 @@ pub fn chart_ui(
         }
         final_dataset.push(
             Dataset::default()
-                .name(all_tx_methods[i].to_owned())
+                .name(all_tx_methods[i].clone())
                 .marker(symbols::Marker::Braille)
                 .graph_type(GraphType::Line)
                 .style(
