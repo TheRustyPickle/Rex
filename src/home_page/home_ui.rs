715c770aa93deb5af7bbcb5944deeffada5406d8
Rusty Pickle <rusty.pickle94@gmail.com>
2024-02-25 03:18:07 +0600

    Update table scrolling

--- src/home_page/home_ui.rs
+++ src/home_page/home_ui.rs
@@ -331,12 +331,22 @@ pub fn home_ui(
         }
     }
 
+    // Always keep some items rendered on the upper side of the table
+    if let Some(index) = table.state.selected() {
+        if index > 10 {
+            *table.state.offset_mut() = index - 10;
+        }
+    }
+
     // after all data is in place, render the widgets one by one
     // the chunks are selected based on the format I want the widgets to render
     f.render_widget(balance_area, chunks[0]);
     f.render_widget(month_tab, chunks[2]);
     f.render_widget(year_tab, chunks[1]);
 
+    
+    
+    
     // this one is different because the Transaction widget interface works differently
     f.render_stateful_widget(table_area, chunks[3], &mut table.state);
 }
