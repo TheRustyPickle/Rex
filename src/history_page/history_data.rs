5ad17cd335da8208f0c54fd623f888a2ebc20f0c
Rusty Pickle <mohammad.sakib0194@gmail.com>
2024-02-29 04:51:22 +0600

    Add fn to fetch activity data

--- src/history_page/history_data.rs
+++ src/history_page/history_data.rs
@@ -2,38 +2,89 @@ use rusqlite::Connection;
 use std::collections::HashMap;
 
 use crate::page_handler::ActivityType;
+use crate::utility::get_all_activities;
 
 pub struct ActivityData {
     created_on: String,
     activity_type: ActivityType,
     description: String,
-    activity_num: usize,
+    activity_num: i32,
+}
+
+impl ActivityData {
+    pub fn new(
+        date: String,
+        activity_type: String,
+        description: String,
+        activity_num: i32,
+    ) -> Self {
+        ActivityData {
+            created_on: date,
+            activity_type: ActivityType::from_str(&activity_type),
+            description,
+            activity_num,
+        }
+    }
+
+    pub fn activity_num(&self) -> i32 {
+        self.activity_num
+    }
 }
 
 pub struct ActivityTx {
     date: String,
     details: String,
     tx_method: String,
-    amount: f64,
+    amount: String,
     tx_type: String,
-    id_num: usize,
     tags: String,
-    activity_num: usize,
+    id_num: String,
+    activity_num: i32,
+    insertion_id: i32,
+}
+
+impl ActivityTx {
+    pub fn new(
+        date: String,
+        details: String,
+        tx_method: String,
+        amount: String,
+        tx_type: String,
+        tags: String,
+        id_num: String,
+        activity_num: i32,
+        insertion_id: i32,
+    ) -> Self {
+        ActivityTx {
+            date,
+            details,
+            tx_method,
+            amount,
+            tx_type,
+            tags,
+            id_num,
+            activity_num,
+            insertion_id,
+        }
+    }
+
+    pub fn activity_num(&self) -> i32 {
+        self.activity_num
+    }
 }
 
 pub struct HistoryData {
-    // Each to be turned into the a table row
     activities: Vec<ActivityData>,
-    // activity num: activity txs
-    // a single activity can affect multiple transactions so a vector for the txs
-    activity_txs: HashMap<usize, Vec<ActivityTx>>,
+    activity_txs: HashMap<i32, Vec<ActivityTx>>,
 }
 
 impl HistoryData {
     pub fn new(month: usize, year: usize, conn: &Connection) -> Self {
-        // Fetch these data from the database
-        // TODO: Two new tables. 1 would contain the activity details. The other table would contain txs and a link to the activity number
-        // In edit and swap mode save both delete and new tx activity, this way we can keep track of both old and the new values
-        todo!()
+        let (activities, activity_txs) = get_all_activities(month, year, conn);
+
+        HistoryData {
+            activities,
+            activity_txs,
+        }
     }
 }
