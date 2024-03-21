821e080d568f9c8e8b5b62b9e94549d5131fc249
Rusty Pickle <rusty.pickle94@gmail.com>
2024-03-21 15:57:29 +0600

    Rename history to activity

--- src/key_checker/activity_keys.rs
+++ src/key_checker/activity_keys.rs
@@ -4,7 +4,7 @@ use crate::key_checker::InputKeyHandler;
 use crate::outputs::HandlingOutput;
 use crate::page_handler::PopupState;
 
-pub fn history_keys(handler: &mut InputKeyHandler) -> Option<HandlingOutput> {
+pub fn activity_keys(handler: &mut InputKeyHandler) -> Option<HandlingOutput> {
     match handler.popup {
         PopupState::Nothing => match handler.key.code {
             KeyCode::Char('q') => return Some(HandlingOutput::QuitUi),
@@ -14,6 +14,7 @@ pub fn history_keys(handler: &mut InputKeyHandler) -> Option<HandlingOutput> {
             KeyCode::Char('h') => handler.do_help_popup(),
             KeyCode::Char('z') => handler.go_summary(),
             KeyCode::Char('w') => handler.go_search(),
+            KeyCode::Char('v') => handler.show_activity_tx_details(),
             KeyCode::Right => handler.handle_right_arrow(),
             KeyCode::Left => handler.handle_left_arrow(),
             KeyCode::Up => handler.handle_up_arrow(),
