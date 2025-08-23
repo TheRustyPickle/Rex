3cd2b0969dc1354e229d1379f94eda6f74960311
Rusty Pickle <rusty.pickle94@gmail.com>
2025-08-23 20:31:31 +0600

    Formatting

--- src/summary_page/mod.rs
+++ src/summary_page/mod.rs
@@ -1,7 +1,7 @@
 mod summary_data;
-mod summary_ui;
 mod summary_models;
+mod summary_ui;
 
 pub use summary_data::SummaryData;
-pub use summary_ui::summary_ui;
 pub use summary_models::*;
+pub use summary_ui::summary_ui;
