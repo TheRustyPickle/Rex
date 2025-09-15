22a009795110b22ff557eb9d71aabc9dfd4a60bb
Rusty Pickle <rusty.pickle94@gmail.com>
2025-09-15 19:06:38 +0600

    Nuke old summary

--- tui/src/summary_page/mod.rs
+++ tui/src/summary_page/mod.rs
@@ -1,9 +1,4 @@
-mod summary_data;
-#[cfg(not(tarpaulin_include))]
-mod summary_models;
 #[cfg(not(tarpaulin_include))]
 mod summary_ui;
 
-pub use summary_data::SummaryData;
-pub use summary_models::*;
 pub use summary_ui::summary_ui;
