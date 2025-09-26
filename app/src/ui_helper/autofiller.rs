e8920448203afddd4bea94af46654f5ea3b27a43
Rusty Pickle <rusty.pickle94@gmail.com>
2025-09-26 19:47:25 +0600

    Update tx type autofiller

--- app/src/ui_helper/autofiller.rs
+++ app/src/ui_helper/autofiller.rs
@@ -1,18 +1,21 @@
 use db::ConnCache;
 use db::models::TxType;
 
-use crate::{conn::MutDbConn, ui_helper::get_best_match};
+use crate::conn::MutDbConn;
+use crate::ui_helper::get_best_match;
 
 pub struct Autofiller<'a> {
     conn: MutDbConn<'a>,
 }
 
+const TX_TYPES: [&str; 3] = ["Income", "Expense", "Transfer"];
+
 impl<'a> Autofiller<'a> {
     pub(crate) fn new(conn: MutDbConn<'a>) -> Self {
-        Autofiller { conn }
+        Self { conn }
     }
 
-    pub fn tx_method(&self, user_input: &str) -> String {
+    pub fn tx_method(self, user_input: &str) -> String {
         let methods = self
             .conn
             .cache()
@@ -50,7 +53,20 @@ impl<'a> Autofiller<'a> {
         } else if trimmed_input.starts_with("i") {
             TxType::Income
         } else {
-            return String::new();
+            let tx_types = TX_TYPES
+                .iter()
+                .map(|s| s.to_string())
+                .collect::<Vec<String>>();
+
+            let best_match = get_best_match(user_input, &tx_types);
+
+            let to_return = if best_match == trimmed_input {
+                String::new()
+            } else {
+                best_match
+            };
+
+            return to_return;
         };
 
         tx_type.to_string()
@@ -89,7 +105,13 @@ impl<'a> Autofiller<'a> {
     }
 
     pub fn details(&self, user_input: &str) -> String {
-        let details = &self.conn.cache().details;
+        let details = self
+            .conn
+            .cache()
+            .details
+            .iter()
+            .map(|d| d.to_string())
+            .collect::<Vec<String>>();
 
         let trimmed_input = user_input.trim();
 
@@ -97,7 +119,7 @@ impl<'a> Autofiller<'a> {
             return String::new();
         }
 
-        let best_match = get_best_match(user_input, details);
+        let best_match = get_best_match(user_input, &details);
 
         if best_match == trimmed_input {
             String::new()
