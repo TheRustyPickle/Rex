3ad1215169b7c1d3f8a7e981a03a4d688624918f
Rusty Pickle <rusty.pickle94@gmail.com>
2025-10-02 16:18:16 +0600

    Update filename

--- tui/src/main.rs
+++ tui/src/main.rs
@@ -42,7 +42,7 @@ fn main() {
         migrated_path.push("rex.sqlite");
 
         // TODO: rename
-        working_path.push("test.sqlite");
+        working_path.push("data.sqlite");
         if initialize_app(&working_path, &migrated_path, &original_dir).is_err() {
             std::process::exit(1);
         }
