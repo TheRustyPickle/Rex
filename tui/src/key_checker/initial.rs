38b4ade2f0ec844b03963cf0646983416c6702f2
Rusty Pickle <rusty.pickle94@gmail.com>
2025-10-11 17:44:44 +0600

    Formatting

--- tui/src/key_checker/initial.rs
+++ tui/src/key_checker/initial.rs
@@ -7,10 +7,12 @@ use crate::pages::PopupType;
 
 /// Tracks the keys of the Initial page and calls relevant function based on it
 pub fn initial_keys(handler: &mut InputKeyHandler) -> Result<Option<HandlingOutput>> {
-    if let PopupType::Nothing = handler.popup_status { match handler.key.code {
-        KeyCode::Char('q') => return Ok(Some(HandlingOutput::QuitUi)),
-        _ => handler.go_home(),
-    } } else {
+    if let PopupType::Nothing = handler.popup_status {
+        match handler.key.code {
+            KeyCode::Char('q') => return Ok(Some(HandlingOutput::QuitUi)),
+            _ => handler.go_home(),
+        }
+    } else {
         if let KeyCode::Char('q') = handler.key.code {
             return Ok(Some(HandlingOutput::QuitUi));
         }
